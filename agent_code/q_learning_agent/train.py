c7904b93e5a58cb44f510d284a30b247ebff0ed2
Jasper Henze <j.henze@stud.uni-heidelberg.de>
2022-03-15 13:46:40 +0100

    agent can now blow up crates and survive as well as collect coins. feature space was increased, priority search function was updated and includes crates and escaping from danger.

--- agent_code/q_learning_agent/train.py
+++ agent_code/q_learning_agent/train.py
@@ -6,19 +6,26 @@ from typing import List
 
 import events as e
 from .callbacks import state_to_features
+from .callbacks import create_additional_feature_states
+from .callbacks import rotate_features
+from .callbacks import mirror_features
+from .callbacks import choose_state
+
 
 # This is only an example!
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
 
 # Hyper parameters -- DO modify
-TRANSITION_HISTORY_SIZE = 3  # keep only ... last transitions
+TRANSITION_HISTORY_SIZE = 1  # keep only ... last transitions
 RECORD_ENEMY_TRANSITIONS = 1.0  # record enemy transitions with probability ...
+LEARNING_RATE = 0.3
 
 # Events
 PLACEHOLDER_EVENT = "PLACEHOLDER"
 APPROACHED_EVENT = 'APPROACHED'
 NAPPROACHED_EVENT = 'NAPPROACHED'
+BOMB_NEAR_CRATE_EVENT = 'BOMB_NEAR_CRATE'
 
 
 def setup_training(self):
@@ -34,12 +41,16 @@ def setup_training(self):
     self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
 
     #setup k-dimensional array where k is the number of features and store an id for each possible state
-    self.state_space = np.arange(0, 5**4)
-    self.state_space = np.reshape(self.state_space, (5, 5, 5, 5))
+    self.state_space = np.arange(0, 6**6)
+    self.state_space = np.reshape(self.state_space, (6, 6, 6, 6, 6, 6))
 
     #setup q table that stores a weight for each possible state and action
     #initialize with ones since positive initial values encourage exploration in the start
-    self.q_table = np.ones((5**4,5))
+    self.q_table = np.ones((6**6, 6))
+
+    self.learning_rate = 0.5
+    self.round = 1
+
 
 
 
@@ -51,14 +62,8 @@ def encode_feature(self, values):
     :param values: features.
     """
     #return the id of the state.
-    #note that features have values starting at -1, hence the +1
-    return self.state_space[values[0] + 1] [values[1] + 1] [values[2] + 1] [values[3] + 1]
-
-def get_q_value(self, id):
-    return self.q_table(id)
-
-def set_q_value(self, id, value):
-    self.q_table[id] = value
+    #note that directional features have values starting at -1, hence the +1
+    return self.state_space[values[0] + 1] [values[1] + 1] [values[2] + 1] [values[3] + 1] [values[4]] [values[5]]
 
 def action_str_to_int(action_string):
     action = 0
@@ -70,6 +75,8 @@ def action_str_to_int(action_string):
         action = 3
     elif action_string == 'WAIT':
         action = 4
+    elif action_string == 'BOMB':
+        action = 5
     return action
 
 
@@ -100,42 +107,28 @@ def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_
         approached = False
         #check for all directions if there was a priority and if that priority was pursued
         #of course this will not be true if we collect a coin but coin collected reward just needs to be big enough
-        for i in range(3):
+        for i in range(4):
             if old_st[i] == 3 and action_str_to_int(self_action) == i:
                 approached = True
         if approached:
             events.append(APPROACHED_EVENT)
         else:
             events.append(NAPPROACHED_EVENT)
+
+        #add event indicating that a bomb was dropped next to a crate:
+        near_crate = False
+        for i in range(4):
+            if old_st[i] == 1 and e.BOMB_DROPPED in events:
+                near_crate = True
+        if near_crate:
+            events.append(BOMB_NEAR_CRATE_EVENT)
     #print(events)
 
 
     # state_to_features is defined in callbacks.py
     self.transitions.append(Transition(old_st, self_action, new_st, reward_from_events(self, events)))
 
-    #set learning and discount rates. Learning rate should ideally be decaying during training
-    learning_rate = 0.3
-    discount = 0.3
-    #update q table for the stored transitions
-    for i in range(len(self.transitions)):
-        if self.transitions[i][2] is None or self.transitions[i][0] is None:
-            break
-
-        action_string = self.transitions[i][1]
-        action = action_str_to_int(action_string)
-
-        old_state = encode_feature(self, self.transitions[i][0])
-        new_state = encode_feature(self, self.transitions[i][2])
-
-        #calculate optimal future value
-        q_values = []
-        for a in range(4):
-            q_values.append(self.q_table[new_state, a])
-        max_q = max(q_values)
-
-        #update q_table
-        self.q_table[old_state, action] = self.q_table[old_state, action] + learning_rate *(self.transitions[i][3] + discount * max_q - self.q_table[old_state, action])
-    self.model = self.q_table
+    update_q_table(self)
 
 
 def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
@@ -154,10 +147,70 @@ def end_of_round(self, last_game_state: dict, last_action: str, events: List[str
     self.logger.debug(f'Encountered event(s) {", ".join(map(repr, events))} in final step')
     self.transitions.append(Transition(state_to_features(last_game_state), last_action, None, reward_from_events(self, events)))
 
+    update_q_table(self)
+
+
+    #self.learning_rate = 0.99 * self.learning_rate
+    self.round += 1
+
     #print(self.model)
     # Store the model
     with open("my-saved-model.pt", "wb") as file:
         pickle.dump(self.model, file)
+
+
+def update_q_table(self):
+    if self.transitions[0][0] is not None and self.transitions[0][2] is not None:
+        #set learning and discount rates. Learning rate should ideally be decaying during training
+        #learning_rate = 0.3
+        discount = 0.8
+        #update q table
+
+        old_features = self.transitions[0][0]
+        new_features = self.transitions[0][2]
+
+        more_old_states = create_additional_feature_states(old_features)
+        more_new_states = create_additional_feature_states(new_features)
+
+        old_st = choose_state(self, more_old_states)
+        new_st = choose_state(self, more_new_states)
+
+        old_state = encode_feature(self, old_st[0])
+        new_state = encode_feature(self, new_st[0])
+
+        action_string = self.transitions[0][1]
+        #translate action according to rotation and mirroring
+        action_string = old_st[1][action_string]
+        action = action_str_to_int(action_string)
+
+        #calculate optimal future value
+        q_values = []
+        for a in range(6):
+            q_values.append(self.q_table[new_state, a])
+        max_q = max(q_values)
+
+        self.q_table[old_state, action] = self.q_table[old_state, action] + self.learning_rate * (self.transitions[0][3] + discount * max_q - self.q_table[old_state, action])
+
+        self.model = self.q_table
+
+    #update for last state of a game
+    if self.transitions[0][2] is None:
+        old_features = self.transitions[0][0]
+
+        more_old_states = create_additional_feature_states(old_features)
+
+        old_st = choose_state(self, more_old_states)
+
+        old_state = encode_feature(self, old_st[0])
+
+        action_string = self.transitions[0][1]
+        #translate action according to rotation and mirroring
+        action_string = old_st[1][action_string]
+        action = action_str_to_int(action_string)
+
+        self.q_table[old_state, action] = self.q_table[old_state, action] + self.learning_rate * (self.transitions[0][3] - self.q_table[old_state, action])
+
+        self.model = self.q_table
 
 
 def reward_from_events(self, events: List[str]) -> int:
@@ -168,12 +221,21 @@ def reward_from_events(self, events: List[str]) -> int:
     certain behavior.
     """
     game_rewards = {
-        e.COIN_COLLECTED: 5,
-        e.KILLED_OPPONENT: 5,
+        e.COIN_COLLECTED: 10,
+        e.KILLED_OPPONENT: 20,
+        e.BOMB_DROPPED: -0.3,
+        e.BOMB_EXPLODED: 0,
+        e.CRATE_DESTROYED: 2,
+        e.COIN_FOUND: 2,
+        e.KILLED_SELF: -20,
+        e.GOT_KILLED: -35,
+        e.OPPONENT_ELIMINATED: 3,
+        e.SURVIVED_ROUND: 10,
         e.INVALID_ACTION : -2,
-        e.WAITED: -0.3,
+        e.WAITED: 0,
         NAPPROACHED_EVENT: -0.5,
-        APPROACHED_EVENT: 0.5
+        APPROACHED_EVENT: 0.5,
+        BOMB_NEAR_CRATE_EVENT: 1
     }
     reward_sum = 0
     for event in events:
